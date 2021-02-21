cc75d08e017a1708ffcaa5ea94d9208682e42ac8
Abdul Wasey <abdulwasey13@gmail.com>
2021-02-21 15:34:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
+import test from './test';
+
+AppRegistry.registerComponent(appName, () => test);
